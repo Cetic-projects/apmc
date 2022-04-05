fe69212286144778debcfe97a03e8bef12c2f1ed
gitsabrinelle <nadour-sabrina@hotmail.com>
2022-04-05 12:27:06 +0200

    install package

---
+++ resources/lang/en/app.php
@@ -0,0 +1,130 @@
+<?php 
+
+return [
+
+	// My controllers
+	"success_store"   => "Item added",
+	"success_update"  => "Updated successfully",
+	"success_destroy" => "Removed successfully",
+
+	// My Views
+	"manage"       => "manage",
+	"edit_title"   => "Edit Item",
+	"delete_title" => "Delete Item",
+	"add_new_item" => "new",
+	"update_item" => "update",
+
+	// View buttons
+	"add_button"    => "Add",
+	"edit_button"    => "Edit",
+
+	// Login, Logout ..etc
+	"start_session"      => "Sign in to start your session",
+	"remember_me"        => "Remember Me",
+	"forgot_password"    => "I forgot my password",
+	"login_btn"          => "Sign In",
+	"reset_password"     => "Reset Password",
+	"reset_password_btn" => "Send reset link",
+	"connect"            => "Loggin",
+
+	// User---------------------------------------
+    //label
+    "user"=>'User',
+    'username'=>"Username",
+    'role'=>'Role',
+    'email'=>'Email',
+    'password'=>'Password',
+    'confirm_password'=>'Confirm password',
+    'address'=>'Address',
+    'website'=>'Website',
+    'company_name'=>'Company name',
+    'about'=>'Description',
+    'status'=>'Status',
+    'phone'=>'Phone number',
+    'city'=>'City',
+    'is_phone_hidden'=>'Visibility of phone',
+    'is_email_hidden'=>'Visibility of email',
+    'nb_stars'=>'Rating',
+    'active'=>'Active',
+    'actions'=>'Actions',
+    'latitude'=>'latitude',
+    'longitude'=>'longitude',
+    'image'=>'Image',
+    'person'=>'person',
+    'icons_social_media'=>'Social networks',
+    'create_date'=>"Register date",
+    "have_not"=>"none",
+    //buttons
+    'delete_list_of_users'=>'Delete selected',
+
+    'users'=>'Users',
+    'products'=>'Products',
+    'posts'=>'Posts',
+    'roles'=>'Roles',
+    'permessions'=>"Permissions",
+    'dashboard'=>'Dashboard',
+
+    'branches'=>'Branches',
+    'branch'=>'Branch',
+    'categories'=>'Categories',
+    'category'=>'Category',
+    'commands'=>'Commands',
+    'command'=>'Command',
+    'announcements'=>'Announcements',
+    'announcement'=>'Announcement',
+    'banners'=>'Banners',
+    'banner'=>'Banner',
+    'employer_box'=>'Employer box',
+    'client_box'=>'Client box',
+    'states'=>'States',
+    'state'=>'State',
+    'cities'=>'Cities',
+    'city'=>'City',
+    'fields'=>'Fields',
+    'field'=>'Field',
+    'optins_field'=>'Field options',
+    'option_field'=>'Field option',
+
+    'name'=>'Name',
+    'responsable'=>'Responsable',
+    'icon'=>'Icon',
+    'in_menu'=>'In menu',
+    'description'=>'Description',
+    'slug'=>'Slug',
+
+
+    'quantity'=>'Quantity',
+    'price'=>'Price',
+    'number_views'=>'Number of vues',
+
+    'product'=>'Product',
+    'approvedBy'=>'Approved by',
+    'receipt'=>'Receipt',
+    'date'=>'Date',
+
+    'position'=>'Position',
+    'start_date'=>'Start date',
+    'end_date'=>'End date',
+    'content'=>'Comment',
+    'rating'=>'Rating',
+    'is_active'=>'Active',
+    'is_required'=>'Is required',
+    'is_reachable'=>"Is reachable",
+    "type"=>"Type",
+    'arabic_name'=>'Name in arabic',
+    'post_code'=>'Poste code',
+    'inbox'=>'Inbox',
+    'trash'=>'Trash',
+    'new_msg'=>'New message',
+    'avatar'=>"Avatar",
+    'value'=>'Content',
+    'begin_promotional_date'=>'Begin promotional date',
+    'end_promotional_date'=>'End promotional date',
+    'bulk_price'=>'Bulk price',
+    'video_url'=>'Video url',
+    'promotional_price'=>'Promotional price',
+    'parent'=>'Parent',
+
+
+
+];
